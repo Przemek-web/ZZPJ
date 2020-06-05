26429fd35e475c1019454392d64144fcf96bcf9e
Przemysław Łapiński <216827@edu.p.lodz.pl>
2020-06-05 13:19:44 +0200

    comment repository test and bean validation

--- src/main/java/pl/lodz/p/it/insta/entities/Comment.java
+++ src/main/java/pl/lodz/p/it/insta/entities/Comment.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,6 +16,8 @@ public class Comment implements Comparable<Comment>{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=1)
     private String content;
     private LocalDateTime addDate;
     @ManyToOne
