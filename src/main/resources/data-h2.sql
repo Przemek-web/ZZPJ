b933ca493cb808036659351dd8942a43a4a3f58f
Przemysław Łapiński <216827@edu.p.lodz.pl>
2020-06-11 13:40:46 +0200

    poprawki do testow

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -4,12 +4,14 @@ INSERT INTO account (username, first_name, last_name, password, email, is_active
 INSERT INTO account (username, first_name, last_name, password, email, is_active) VALUES ('$$VADER$$', 'Anakin', 'Skywalker', '$2y$12$3fd99QFLRdLLDJyKB3OtGe2T6paUS66hePAzMEUWAX4yyyJxOAT/S','vader@02.pl',true);
 INSERT INTO account (username, first_name, last_name, password, email, is_active) VALUES ('Ksiezniczka123', 'Leia', 'Organa', '$2y$12$3fd99QFLRdLLDJyKB3OtGe2T6paUS66hePAzMEUWAX4yyyJxOAT/S','leia@o2.pl',true);
 
-INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/jonsnowseedog.jpg'),'Fajny pieseł, chyba nazwę go Duch',{ts '2002-09-17 18:47:52.69'},1);
-INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/firsttimewithygritte.jpg'),'Złapałem jakąś dzikuske, mówi że nazywa się Ygritte',{ts '2004-09-17 18:47:52.69'},1);
-INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/jonsnowoathwithsam.jpg'),'Przysięga z Samem do nocnej straży',{ts '2003-09-17 18:47:52.69'},1);
-INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/jonsnofirsttimeonwall.jpg'),'Pierwszy raz na murze, ale wysoko',{ts '2003-08-17 18:47:52.69'},1);
+INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/jonsnowseedog.jpg'),'Fajny pieseł, chyba nazwę go Duch',{ts '2012-09-17 18:47:52.69'},1);
+INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/firsttimewithygritte.jpg'),'Złapałem jakąś dzikuske, mówi że nazywa się Ygritte',{ts '2014-09-17 18:47:52.69'},1);
+INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/jonsnowoathwithsam.jpg'),'Przysięga z Samem do nocnej straży',{ts '2013-09-17 18:47:52.69'},1);
+INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/jonsnofirsttimeonwall.jpg'),'Pierwszy raz na murze, ale wysoko',{ts '2013-12-17 18:47:52.69'},1);
 
-INSERT INTO comment (content, add_date, account_id, post_id) VALUES ('Ehh kiedyś to było, teraz to strzela do mnie z łuku...', {ts '2005-02-17 18:47:52.69'},1,2);
+INSERT INTO comment (content, add_date, account_id, post_id) VALUES ('Ehh kiedyś to było, teraz to strzela do mnie z łuku...', {ts '2015-02-17 18:47:52.69'},1,2);
+INSERT INTO comment (content, add_date, account_id, post_id) VALUES ('Komentarz 2 ', {ts '2015-04-17 18:47:52.69'},1,2);
+INSERT INTO comment (content, add_date, account_id, post_id) VALUES ('Komentarz 3 ', {ts '2015-03-17 18:47:52.69'},1,2);
 
 INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/ben1.jpg'),'Ale jestem przystojniacha',{ts '1977-07-19 13:47:32.69'},2);
 INSERT INTO post (lob, description, add_date,account_id) VALUES (FILE_READ('classpath:images/Ben_Kenobi.jpg'),'To ja staje do walki',{ts '1977-07-19 18:47:32.69'},2);
